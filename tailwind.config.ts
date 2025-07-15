e1a0c063d04f3b336fcdf2db76186d5338c87b54
Sambat Das <revosambat123@gmail.com>
2025-07-15 22:08:12 +0545

    tailwind configuration for responsive classes added on screens and future scope for creating theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,27 @@
 // tailwind.config.ts
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-    screens: {
-      xs: '400px',     // Extra small (custom)
-      sm: '640px',     // Small (default)
-      md: '768px',     // Medium
-      lg: '1024px',    // Large
-      xl: '1280px',    // Extra Large
-      '2xl': '1536px', // Extra Extra Large
-    },
-  },
-  plugins: [],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
+		extend: {},
+		screens: {
+			xs: "400px", // Extra small (custom)
+			sm: "640px", // Small (default)
+			md: "768px", // Medium
+			lg: "1024px", // Large
+			xl: "1280px", // Extra Large
+			"2xl": "1536px", // Extra Extra Large
+		},
+	},
+	plugins: [],
 }
 
 export default config
