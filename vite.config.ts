bf929a8da1ae0b3dd03a181680a307e05fba4ea4
Sambat Das <revosambat123@gmail.com>
2025-07-22 14:35:07 +0545

    fixes for pushing into prodution using gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	base: '/item-manager-portal/'
+	base: "/item-manager-portal/",
+	build: {
+		outDir: "dist",
+	},
 })
